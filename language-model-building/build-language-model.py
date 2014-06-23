9c85d97bcf80f1f87e6109e7ab97f10edf527c25
Martin Thoma <info@martin-thoma.de>
2014-06-23 08:10:09 -0400

    some progress in building a very simple language modell

--- language-model-building/build-language-model.py
+++ language-model-building/build-language-model.py
@@ -1,11 +1,108 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+import logging
+FORMAT = "%(asctime)-15s %(message)s"
+logging.basicConfig(filename='example.log',
+                    level=logging.DEBUG,
+                    format=FORMAT)
+logging.debug("Test")
 import re
 import urllib2
 import MySQLdb
 import MySQLdb.cursors
 from dbconfig import mysql
-import sys
+import os
+import glob
+import fnmatch
+
+
+def extract_wikipedia_article(article_name):
+    content  = urllib2.urlopen('http://de.wikipedia.org/w/api.php?format=json&action=query&titles=Laplace-Operator&prop=revisions&rvprop=content').read()
+    mathexpressions = extract_math_mode(content, is_wikipedia=True)
+    for expr in mathexpressions:
+        expr = expr.replace("\\\\", "\\")
+        symbols = extract_symbols(expr)
+        for symbol in symbols:
+            print(symbol+" in "+expr)
+
+
+def get_known_symbols():
+    sql = ("SELECT  `id` ,  `formula_in_latex`  FROM  `wm_formula` "
+           "WHERE formula_type = 'single symbol';")
+    cursor.execute(sql)
+    datasets = cursor.fetchall()
+    known_symbols = {}
+    for el in datasets:
+        known_symbols[el['formula_in_latex']] = 0
+    return known_symbols
+
+
+def extract_by_deleting(fn, known_symbols={}):
+    basepath = os.path.dirname(fn) + "/"
+
+    with open(fn, "rb") as f:
+        content = f.read().strip()
+
+    content_tmp = replace_definitions(content, basepath)
+    while content != content_tmp:
+        content = content_tmp
+        content_tmp = replace_definitions(content, basepath)
+    symbollist = []
+    for symbol, count in known_symbols.items():
+        symbollist.append(symbol)
+    # letters, numbers and some special characters can be part of commands and
+    # thus they should only be used in a much more sophisticated script
+    small_letters = map(chr, range(ord('a'), ord('z')+1))
+    big_letters = map(chr, range(ord('A'), ord('Z')+1))
+    digits = map(chr, range(ord('0'), ord('9')+1))
+    special = ['[', ']', '{', '}', '\\', ',', '.', ';', '.']
+    symbollist = sorted(symbollist, key=lambda x: len(x), reverse=True)
+
+    for symbol in symbollist:
+        if symbol in (small_letters+big_letters+digits+special):
+            continue
+        pattern = re.escape(symbol)+"(?=[^A-Za-z]+)"
+        symbolreg = re.compile(pattern)
+        hits = symbolreg.findall(content)
+        if len(hits) > 0:
+            known_symbols[symbol] += len(hits)
+            content = re.sub(symbolreg,
+                             lambda m: "####",
+                             content)
+    return known_symbols
+
+
+def replace_definitions(text, basepath):
+    # self defined \usepackage
+    usepackage = re.compile(r"\\usepackage\{(.*?)\}")
+    for filename in usepackage.findall(text):
+        filename = basepath + filename + ".sty"
+        if os.path.isfile(filename):
+            with open(filename) as f:
+                c = f.read()
+            text = re.sub(usepackage,
+                          lambda m: c,
+                          text)
+
+    # \input
+    inputcode = re.compile(r"\\input\{(.*?)\}")
+    text = re.sub(inputcode,
+                  lambda m: get_file_content(basepath+str(m.group(1))),
+                  text)
+
+    # \newcommand
+    newcommand = re.compile(r"\\newcommand\{(.*?)\}\s*\{(.*?)\}")
+    for findtext, replacetext in newcommand.findall(text):
+        text = text.replace(findtext, replacetext)
+
+    # \def
+    defcommand = re.compile(r"\\def(\\[A-Za-z][A-Za-z0-9]*)\{((?:[^\{]*?(?:\{(?:.*?)\}){0,3})+)\}")
+    for findtext, replacetext in defcommand.findall(text):
+        text = text.replace(findtext, replacetext)
+    # remove the command itself
+    text = re.sub(defcommand, lambda m: "", text)
+    return text
 
 
 def extract_math_mode(text, is_wikipedia=False):
@@ -37,187 +134,54 @@ def extract_document_body(text):
     return extract_environments('document', text)
 
 
-def extract_wikipedia_article(article_name):
-    content  = urllib2.urlopen('http://de.wikipedia.org/w/api.php?format=json&action=query&titles=Laplace-Operator&prop=revisions&rvprop=content').read()
-    mathexpressions = extract_math_mode(content, is_wikipedia=True)
-    for expr in mathexpressions:
-        expr = expr.replace("\\\\", "\\")
-        symbols = extract_symbols(expr)
-        for symbol in symbols:
-            print(symbol+" in "+expr)
+def get_file_content(filename):
+    if os.path.isfile(filename):
+        with open(filename, "rb") as f:
+            content = f.read()
+    elif os.path.isfile(filename+".tex"):
+        with open(filename+".tex", "rb") as f:
+            content = f.read()
+    else:
+        logging.warning("File '%s' and '%s.tex' does not exist." %
+                        (filename, filename))
+        content = ""
+    return content
 
 
-def tokenize(mathexpression, tokens=None):
-    if tokens is None:
-        tokens = []
-    if '{' in mathexpression:
-        brakets = re.compile('{}')
-    return tokens
-
-
-def extract_latex_document(fn='/home/moose/Downloads/1406.5173v1_FILES/ms.tex',
-                           known_symbols={}):
-    with open(fn) as f:
-        content = [f.read().strip()]
-    c2 = []
-
-    while len(content) > 0 or len(c2) > 0:
-        if len(content) == 0:
-            content = c2
-            c2 = []
-        for mathel in content:
-            if mathel in known_symbols:
-                known_symbols[mathel] += 1
-                continue
-            for el in extract_math_mode(mathel):
-                # Nesting structures
-                if r"\frac{" in el:
-                    pattern = re.compile(r'(.*)\\frac{(.*?)}{(.*?)}(.*)',
-                                         re.MULTILINE | re.DOTALL)
-                    matches = pattern.findall(el)
-                    for match in matches:
-                        for el in match:
-                            if el != '':
-                                c2.append(el.strip())
-                elif "^{" in el:
-                    pattern = re.compile('(.*)\^{(.*)}(.*)')
-                    matches = pattern.findall(el)
-                    for base, head, after in matches:
-                        if base+"^{"+head+"}"+after != el:
-                            print("Not equal^!"*12)
-                            print("El: %s" % el)
-                            print("El: %s" % base+"^{"+head+"}"+after)
-                            print("base: %s" % base)
-                            print("head: %s" % head)
-                            print("after: %s" % after)
-                            sys.exit(-1)
-                        if base != '' and base is not None:
-                            c2.append(base.strip())
-                        if head != '' and head is not None:
-                            c2.append(head.strip())
-                        if after != '' and head is not None:
-                            c2.append(after.strip())
-                elif "_{" in el:
-                    pattern = re.compile('(.*)\_{(.*)}(.*)')
-                    matches = pattern.findall(el)
-                    for base, head, after in matches:
-                        if base+"_{"+head+"}"+after != el:
-                            print("Not equal_!"*12)
-                            print("El: %s" % el)
-                            print("El: %s" % base+"^{"+head+"}"+after)
-                            print("base: %s" % base)
-                            print("head: %s" % head)
-                            sys.exit(-1)
-                        if base != '' and base is not None:
-                            c2.append(base.strip())
-                        if head != '' and head is not None:
-                            c2.append(head.strip())
-                        if after != '' and head is not None:
-                            c2.append(after.strip())
-                elif "{" in el:
-                    print("There is still a '{'. That should not happen.")
-                    sys.exit(-1)
-                elif "}" in el:
-                    print("There is still a '}'. That should not happen.")
-                    sys.exit(-1)
-                elif "1" in el:
-                    pattern = re.compile('(.*?)1(.*)')
-                    matches = pattern.findall(el)
-                    for base, head in matches:
-                        if base+"1"+head != el:
-                            print("Not equal_!"*12)
-                            print("El: %s" % el)
-                            print("El: %s" % base+"1"+head+"")
-                            print("base: %s" % base)
-                            print("head: %s" % head)
-                            sys.exit(-1)
-                        if base != '' and base is not None:
-                            c2.append(base.strip())
-                        if head != '' and head is not None:
-                            c2.append(head.strip())
-                elif "8" in el:
-                    pattern = re.compile('(.*?)8(.*)')
-                    matches = pattern.findall(el)
-                    for base, head in matches:
-                        if base+"8"+head != el:
-                            print("Not equal_!"*12)
-                            print("El: %s" % el)
-                            print("El: %s" % base+"1"+head+"")
-                            print("base: %s" % base)
-                            print("head: %s" % head)
-                            sys.exit(-1)
-                        if base != '' and base is not None:
-                            c2.append(base.strip())
-                        if head != '' and head is not None:
-                            c2.append(head.strip())
-                elif " " in el:
-                    pattern = re.compile('(.*?) (.*)')
-                    matches = pattern.findall(el)
-                    for base, head in matches:
-                        if base + " " + head != el:
-                            print("Not equal_!"*12)
-                            print("El: %s" % el)
-                            print("El: %s" % base+" "+head+"")
-                            print("base: %s" % base)
-                            print("head: %s" % head)
-                            sys.exit(-1)
-                        if base != '' and base is not None:
-                            c2.append(base.strip())
-                        if head != '' and head is not None:
-                            c2.append(head.strip())
-                elif "-" in el:
-                    pattern = re.compile('(.*?)-(.*)')
-                    matches = pattern.findall(el)
-                    for base, head in matches:
-                        if base + "-" + head != el:
-                            print("Not equal-!"*12)
-                            print("El: %s" % el)
-                            print("El: %s" % base+"-"+head+"")
-                            print("base: %s" % base)
-                            print("head: %s" % head)
-                            sys.exit(-1)
-                        if base != '' and base is not None:
-                            c2.append(base.strip())
-                        if head != '' and head is not None:
-                            c2.append(head.strip())
-                elif el in known_symbols:
-                    known_symbols[el] += 1
-                else:
-                    print("unknown symbol: %s" % el)
-                    known_symbols[el] = 1  # TODO: Really?
-        content = []
-    return known_symbols
-
-
-def get_known_symbols():
-    sql = ("SELECT  `id` ,  `formula_in_latex`  FROM  `wm_formula` "
-           "WHERE formula_type = 'single symbol';")
-    cursor.execute(sql)
-    datasets = cursor.fetchall()
-    known_symbols = {}
-    for el in datasets:
-        known_symbols[el['formula_in_latex']] = 0
-    return known_symbols
-
-
-def main():
-    known_symbols = get_known_symbols()
-    results = extract_latex_document(known_symbols=known_symbols)
-    print("#"*80)
-    print("Results:")
-    for latex, counter in sorted(results.items(), key=lambda x: x[1], reverse=True):
-        if counter > 0:
-            print("%s: %i" % (latex, counter))
-        if "frac" in latex:
-            print latex
-            print "#"*200
+def is_latex_root(filename):
+    with open(filename) as f:
+        content = f.read()
+    return "\\documentclass" in content
 
 
 if __name__ == "__main__":
+    logging.info("Start establishing connection")
     connection = MySQLdb.connect(host=mysql['host'],
                                  user=mysql['user'],
                                  passwd=mysql['passwd'],
                                  db=mysql['db'],
                                  cursorclass=MySQLdb.cursors.DictCursor)
+    logging.info("Got it. Get cursor.")
     cursor = connection.cursor()
-    main()
+    logging.info("connection established")
+    logging.info("Start getting symbols")
+    known_symbols = get_known_symbols()
+    logging.info("Got symbols")
+    matches = []
+    for root, dirnames, filenames in os.walk('/home/moose/Downloads/LaTeX-examples/'):
+      for filename in fnmatch.filter(filenames, '*.tex'):
+          matches.append(os.path.join(root, filename))
+    for filename in matches:
+        if is_latex_root(filename):
+            print("yes"+filename)
+            # filename = "/home/moose/Downloads/LaTeX-examples/documents/GeoTopo/Kapitel1.tex"
+            # filename = "/home/moose/Downloads/1406.5173v1_FILES/ms.tex"
+            # filename = "/home/moose/Downloads/LaTeX-examples/documents/GeoTopo/GeoTopo.tex"
+            known_symbols = extract_by_deleting(filename, known_symbols=known_symbols)
+    logging.info("#"*80)
+    logging.info("Results:")
+    for latex, counter in sorted(known_symbols.items(),
+                                 key=lambda x: x[1],
+                                 reverse=True):
+        if counter > 0:
+            logging.info("%s: %i" % (latex, counter))
---
+++ language-model-building/latex-parser.py
@@ -0,0 +1,287 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from argparse import ArgumentParser
+import sys
+
+
+class Tree(object):
+    def __init__(self, name, parent):
+        self.name = name
+        self.nodes = []
+        self.parent = parent
+        self.options = ""
+        self.arguments = []
+        self.is_environment = False
+        if parent is None:
+            self.is_mathmode = False
+        else:
+            self.is_mathmode = parent.is_mathmode
+        self.is_displaystyle = False
+        self.is_comment = False
+
+    def append(self, new_node):
+        assert isinstance(new_node, Tree)
+        self.nodes.append(new_node)
+
+    def __str__(self):
+        if self.is_environment:
+            out = "Environment: %s" % (self.name)
+        else:
+            out = "Node: %s" % (self.name)
+
+        if self.is_mathmode and not self.is_displaystyle:
+            out += " (mathmode)"
+        elif self.is_mathmode and self.is_displaystyle:
+            out += " (mathmode, displaystyle)"
+
+        if self.options.strip() != "":
+            out += "[options:%s]" % self.options
+
+        for arg in self.arguments:
+            out += "{arg:%s} " % str(arg)
+        out += "\n"
+        indent = " " * 4
+        for node in self.nodes:
+            for line in str(node).split('\n'):
+                out += indent + line + "\n"
+            out += "\n"
+        return out.strip()
+
+
+class Parser:
+    def __init__(self, content):
+        self.content = content
+        self.symbol_table = {}
+        self.root = Tree("Main", None)
+        self._current_node = self.root
+        self._command_name_started = False
+        self._strbuffer = ""
+        self._environment_name_started = False
+        self._argument_started = False
+        self._command_options = True
+        self._readchar = -1
+        self._options_started = False
+        self._command_arguments = {'\\newcommand': 2, '\\documentclass': 1}
+
+    def _append_command(self):
+        new_node = Tree(self._strbuffer, self._current_node)
+        self._current_node.append(new_node)
+        self._current_node = new_node
+
+    def _ascend(self):
+        self._current_node = self._current_node.parent
+
+    def _print_status(self, line, char):
+        print("This should not happen (line %i)" % line)
+        print("strbuffer:\t%s" % self._strbuffer)
+        print("char:\t%s" % char)
+        print("readchar:\t%i" % self._readchar)
+        print("### State")
+        print("current node name:\t%s" % self._current_node.name)
+        print("options_started:\t%r" % self._options_started)
+        print("arguments_started:\t%r" % self._argument_started)
+        print("command_name_started:\t%r" % self._command_name_started)
+        print("is_mathmode:\t\t%r" % self._current_node.is_mathmode)
+        print("is_displaystyle:\t%r" % self._current_node.is_displaystyle)
+        print("### Root")
+        print(self.root)
+        sys.exit(-1)
+
+    def _handle_backslash(self):
+        if self._command_name_started:
+            if self._strbuffer == "\\":
+                # It's a \\
+                self._strbuffer += "\\"
+                self._append_command()
+                self._strbuffer = ""
+                self._command_name_started = False
+                if self._environment_name_started or \
+                   self._command_options:
+                    self._print_status(90, "\\")
+            else:
+                # The name of the command is now over and a new command
+                # begins
+                self._append_command()
+                self._strbuffer = "\\"
+        else:
+            # No command started so far
+            self._command_name_started = True
+            if self._strbuffer != '':
+                new_node = Tree(self._strbuffer, self._current_node)
+                self._current_node.append(new_node)
+            self._strbuffer = "\\"
+
+    def _handle_dollar(self):
+        if self._strbuffer == "\\":
+            # escaped dollar sign
+            self._strbuffer += "$"
+        else:
+            # It's not escapted
+            if self._strbuffer == "$":
+                self._strbuffer += "$"
+                if self._current_node.name == "$$":
+                    self._ascend()
+                else:
+                    self._print_status(123, "double dollar ($$)")
+            else:
+                new_node = Tree(self._strbuffer, self._current_node)
+                if self._current_node.is_mathmode:
+                    self._current_node.append(new_node)
+                    self._ascend()
+                    self._strbuffer = ""
+                else:
+                    self._current_node.append(new_node)
+                    new_node = Tree("$", self._current_node)
+                    new_node.is_mathmode = True
+                    self._current_node.append(new_node)
+                    self._strbuffer = ""
+            self._command_name_started = False
+
+    def _handle_open_bracket(self):
+        """ for '[' """
+        if self._current_node.is_mathmode:
+            self._strbuffer += "["
+        elif self._strbuffer == "\\":
+            self._append_command()
+            self._current_node.is_mathmode = True
+            self._current_node.is_displaystyle = True
+        elif self._command_name_started:
+            # Add the new command to the tree
+            self._append_command()
+            # Start recording its options
+            self._strbuffer = ""
+            self._command_options = True
+            self._command_name_started = False
+        else:
+            # It was something different, e.g. math mode
+            self._strbuffer += "["
+            self._print_status(122, "[")
+
+    def _handle_close_bracket(self):
+        if self._current_node.is_mathmode:
+            if self._strbuffer == "\\":
+                # Finished math mode
+                #self._ascend
+                pass
+            else:
+                self._strbuffer += "]"
+        elif self._command_options:
+            # The options ended
+            self._current_node.options = self._strbuffer
+            self._strbuffer = ""
+            self._command_options = False
+        else:
+            # It was something different, e.g. math mode
+            self._strbuffer += "]"
+
+    def _handle_space(self):
+        self._current_node.append(Tree(self._strbuffer, self._current_node))
+        self._strbuffer = ""
+        self._command_name_started = False
+        if self._environment_name_started:
+            self._print_status(self, 146, " ")
+
+    def _handle_open_curly(self):
+        if self._command_name_started:
+            if self._strbuffer == "\\begin":
+                self._environment_name_started = True
+            elif self._strbuffer == "\\end":
+                self._environment_name_started = False
+            else:
+                self._argument_started = True
+                self._append_command()
+                self._strbuffer = ""
+                self._command_name_started = False
+        if self._environment_name_started:
+            self._strbuffer = ""
+        else:
+            self._argument_started = True
+
+    def _handle_close_curly(self):
+        if self._command_name_started:
+            if self._strbuffer == "\\begin":
+                self._environment_name_started = True
+            elif self._strbuffer == "\\end":
+                self._environment_name_started = False
+            elif self._argument_started:
+                self._current_node.arguments.append(self._strbuffer)
+                self._command_name_started = False
+                self._argument_started = False
+
+                tmp = self._command_arguments[self._current_node.name]
+                if len(self._current_node.arguments) == tmp:
+                    self._ascend()
+            else:
+                self._append_command()
+                self._strbuffer = ""
+                #self._ascend()
+        elif self._environment_name_started:
+            self._environment_name_started = False
+            self._append_command()
+            self._strbuffer = ""
+        elif self._argument_started:
+            self._current_node.arguments.append(self._strbuffer)
+            self._argument_started = False
+            self._strbuffer = ""
+            tmp = self._command_arguments[self._current_node.name]
+            if len(self._current_node.arguments) == tmp:
+                self._ascend()
+        else:
+            self._print_status(210, "}")
+
+    def _handle_percentage(self):
+        if self._strbuffer.endswith("\\"):
+            self._strbuffer += "%"
+        else:
+            self._current_node.is_comment = True
+
+    def _handle_newline(self):
+        if self._current_node.is_comment:
+            #self._ascend
+            pass
+
+    def parse(self):
+        for i, char in enumerate(self.content):
+            self._readchar = i
+            if char == "\\":  # This is an escaped single slash
+                self._handle_backslash()
+            elif char == "$":
+                self._handle_dollar()
+            elif char == "[":
+                self._handle_open_bracket()
+            elif char == "]":
+                self._handle_close_bracket()
+            elif char == " ":
+                self._handle_space()
+            elif char == "{":
+                self._handle_open_curly()
+            elif char == "}":
+                self._handle_close_curly()
+            elif char == "%":
+                self._handle_percentage()
+            elif char == "\n":
+                self._handle_newline()
+            else:
+                self._strbuffer += char
+        return self.symbol_table, self.root
+
+
+def parse(filename):
+    with open(filename) as f:
+        content = f.read().strip()
+    p = Parser(content[:150])
+    return p.parse()
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+
+    filename = "/home/moose/Downloads/1406.5173v1_FILES/ms.tex"
+    # Add more options if you like
+    parser.add_argument("-f", "--file", dest="filename",
+                        default=filename,
+                        help="write report to FILE", metavar="FILE")
+    args = parser.parse_args()
+    symbol_table, parsed = parse(args.filename)
+    print(symbol_table)
+    print(parsed)
