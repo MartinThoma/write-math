ada585c8955370fe3df3b2627fa97a59183ca9e0
Martin Thoma <info@martin-thoma.de>
2014-06-29 02:26:40 -0400

    added json_msg function

--- website/classify/functions.php
+++ website/classify/functions.php
@@ -1,5 +1,20 @@
 <?php
 
+if (!function_exists('json_last_error_msg')) {
+    function json_last_error_msg() {
+        static $errors = array(
+            JSON_ERROR_NONE             => null,
+            JSON_ERROR_DEPTH            => 'Maximum stack depth exceeded',
+            JSON_ERROR_STATE_MISMATCH   => 'Underflow or the modes mismatch',
+            JSON_ERROR_CTRL_CHAR        => 'Unexpected control character found',
+            JSON_ERROR_SYNTAX           => 'Syntax error, malformed JSON',
+            JSON_ERROR_UTF8             => 'Malformed UTF-8 characters, possibly incorrectly encoded'
+        );
+        $error = json_last_error();
+        return array_key_exists($error, $errors) ? $errors[$error] : "Unknown error ({$error})";
+    }
+}
+
 function insert_userdrawing($user_id, $data) {
     global $pdo, $msg;
 
