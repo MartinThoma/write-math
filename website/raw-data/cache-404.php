256ece39e708f87c69eb534fe538b26a2e0df4e4
Martin Thoma <info@martin-thoma.de>
2014-06-29 20:18:01 -0400

    added 404 script

---
+++ website/raw-data/cache-404.php
@@ -0,0 +1,15 @@
+<?php
+include '../init.php';
+require_once '../svg.php';
+$raw_data_id = basename($_SERVER['REQUEST_URI'], ".svg");
+
+
+$sql = "SELECT `data` FROM `wm_raw_draw_data` WHERE id=:did";
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(':did', $raw_data_id, PDO::PARAM_INT);
+$stmt->execute();
+$data = $stmt->fetchObject()->data;
+
+create_raw_data_svg($raw_data_id, $data);
+header("Location: ../raw-data/$raw_data_id.svg");
+?>
