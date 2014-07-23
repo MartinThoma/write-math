8278eae71e2e222258d4f84571cd898c03d37e64
Martin Thoma <info@martin-thoma.de>
2014-07-23 02:51:06 -0400

    improved tools to find / fix errors

--- find-errors/README.md
+++ find-errors/README.md
@@ -1,1 +1,5 @@
-Scripts in this folder are used to find errors in the dataset.
+Scripts in this folder are used to find and/or correct errors in the dataset.
+
+* `fix_json_errors.py`: Runs automatically over the whole dataset and corrects
+                        JSON strings (raw_data) that are wrong.
+* `find_errors.py`: 
--- find-errors/find_errors.py
+++ find-errors/find_errors.py
@@ -179,7 +179,7 @@ class HandwrittenDataM(HandwrittenData):
 
         def unaccept_function(a):
             self.unaccept = True
-            print("unaccept")
+            print("unaccept raw_data_id %i" % self.raw_data_id)
             plt.close()
         unacceptpos = plt.axes([0.81, 0.05, 0.1, 0.075])
         unaccept_button = Button(unacceptpos, 'unaccept')
@@ -187,7 +187,7 @@ class HandwrittenDataM(HandwrittenData):
 
         def is_trash_function(a):
             a.istrash = True
-            print("trash")
+            print("trash raw_data_id %i" % self.raw_data_id)
             plt.close()
         is_trash_pos = plt.axes([0.7, 0.2, 0.1, 0.075])
         is_trash_button = Button(is_trash_pos, 'is_trash')
@@ -226,6 +226,9 @@ def main(raw_data_start_id):
                            #   'KIND': 'cubic'})
                            ]
 
+    checked_formulas = 0
+    checked_raw_data_instances = 0
+
     for formula_id in formulaid2latex.keys():
         alread_shown_in_browser = False
         if formula_id == 1:
@@ -240,14 +243,20 @@ def main(raw_data_start_id):
                "`has_too_long_line`, `is_image`, `administrator_edit`, "
                "`other_problem`, `has_interrupted_line` "
                "FROM  `wm_raw_draw_data` "
-               "WHERE `accepted_formula_id` = %i;" % formula_id)
+               "WHERE `accepted_formula_id` = %i "
+               "ORDER BY `administrator_edit` DESC, "
+               "`creation_date` ASC;") % formula_id
         cursor.execute(sql)
         raw_datasets = cursor.fetchall()
         print("Raw datasets: %i" % len(raw_datasets))
+        checked_raw_data_instances += len(raw_datasets)
+        checked_formulas += 1
         if len(raw_datasets) < 100:
             continue
         As = []
         for i, data in enumerate(raw_datasets):
+            if data['data'] == "[]":
+                continue
             B = HandwrittenDataM(data['data'],
                                  data['accepted_formula_id'],
                                  data['wild_point_count'],
@@ -277,6 +286,9 @@ def main(raw_data_start_id):
                         update_data(B)
                     else:
                         update_data(B, True)
+        print("[Status] Checked formulas: %i of %i" % (checked_formulas,
+                                                       len(formulaid2latex)))
+        print("[Status] Checked raw_data_instances: %i" % checked_raw_data_instances)
     print("done")
 
 if __name__ == '__main__':
