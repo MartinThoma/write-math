9dc98551588ffb62f941cd336ec802570e3dede8
Martin Thoma <info@martin-thoma.de>
2014-07-28 12:40:13 -0400

    added help

--- website/clients/python/show_data.py
+++ website/clients/python/show_data.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python
+"""
+Print which symbols are contained in a datasets.pickle file.
+"""
 
 import cPickle as pickle
 
-a = pickle.load(open('cv_datasets.pickle'))
-s = ""
-for symbol, count in sorted(a['symbols'].items(), key=lambda n: n[0]):
-    if symbol in ['a', '0', 'A']:
-        s += "\n%s (%i), " % (symbol, count)
-    elif symbol in ['z', '9', 'Z']:
-        s += "%s (%i) \n" % (symbol, count)
-    else:
-        s += "%s (%i), " % (symbol, count)
-print("```")
-print(s)
-print("```")
+
+def main(picklefile):
+    a = pickle.load(open(picklefile))
+    s = ""
+    for symbol, count in sorted(a['symbols'].items(), key=lambda n: n[0]):
+        if symbol in ['a', '0', 'A']:
+            s += "\n%s (%i), " % (symbol, count)
+        elif symbol in ['z', '9', 'Z']:
+            s += "%s (%i) \n" % (symbol, count)
+        else:
+            s += "%s (%i), " % (symbol, count)
+    print("```")
+    print(s)
+    print("```")
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument("-f", "--file", dest="picklefile",
+                        default='cv_datasets.pickle',
+                        help="where is the picklefile", metavar="FILE")
+    args = parser.parse_args()
+    main(args.picklefile)
