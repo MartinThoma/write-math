07682d6fec880fb150f1592c55e39b3aec77689a
Martin Thoma <info@martin-thoma.de>
2014-07-31 17:17:24 -0400

    simplified dtw_classifiert to use softmax; put greedy matching algorithm in own file

--- website/clients/dtw-python/dtw_classifier.py
+++ website/clients/dtw-python/dtw_classifier.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
-from math import sqrt, exp
-import logging
 import HandwrittenData
+import distance_metric
 
 
 class dtw_classifier(object):
@@ -34,60 +33,12 @@ class dtw_classifier(object):
 
         assert type(A) is HandwrittenData.HandwrittenData
 
-        def dtw(A, B, simple=True, SQUARED=True):
-            """ Calculate the distance of A and B by greedy dynamic time
-            warping.
-            @param  list A list of points
-            @param  list B list of points
-            @return float  Minimal distance you have to move points from A to
-                           get B
-            """
-            assert type(A) is list
-            assert type(B) is list
-
-            def distance(p1, p2, squared=False):
-                """ Calculate the squared eucliden distance of two points.
-                @param  associative array $p1 first point
-                @param  associative array $p2 second point
-                @return float
-
-                >>> distance({'x': 0, 'y': 0}, {'x': 3, 'y': 4})
-                5.0
-                >>> '%.2f' % distance({'x': 0, 'y': 0}, {'x': 1, 'y': 22})
-                '22.02'
-                """
-                dx = p1["x"] - p2["x"]
-                dy = p1["y"] - p2["y"]
-                if squared:
-                    return (dx*dx + dy*dy)
-                else:
-                    return sqrt(dx*dx + dy*dy)
-
-            distanceSum = 0.0
-            if isinstance(A[0], list):
-                if len(A) != len(B):
-                    return float("inf")
-                else:
-                    from Dtw import Dtw
-                    for lineA, lineB in zip(A, B):
-                        a = Dtw(lineA[:],
-                                lineB,
-                                lambda a, b: distance(a, b, SQUARED)
-                                )
-                        distanceSum += a.calculate(simple)
-            else:
-                from Dtw import Dtw
-                a = Dtw(A[:], B, lambda a, b: distance(a, b, SQUARED))
-                distanceSum += a.calculate(simple)
-
-            return distanceSum
-
         results = []
-        A = A.get_pointlist()
 
         for key, dataset in enumerate(self.datasets):
-            B = dataset['handwriting'].get_pointlist()
-            results.append(dict({"dtw": dtw(A, B)}.items() + dataset.items()))
+            B = dataset['handwriting']
+            d = distance_metric.handwritten_data_greedy_matching_distance(A, B)
+            results.append(dict({"dtw": d}.items() + dataset.items()))
 
         results = sorted(results, key=lambda k: k['dtw'])
         results = filter(lambda var: var['dtw'] < self.THRESHOLD, results)
@@ -100,32 +51,19 @@ class dtw_classifier(object):
             else:
                 results2[row['formula_id']] = row['dtw']
 
-        results = [{'formula_id': key, 'dtw': el} for key, el in results2.items()]
+        results = [{'formula_id': key, 'dtw': el}
+                   for key, el in results2.items()]
         results = sorted(results, key=lambda k: k['dtw'])[:10]
 
         def get_probability_from_distance(results):
             """ Get a list of results with dtw and formula id and return a
                 dict mapping formula-ids to probabilities.
             """
-            # check if one distance is 0 and meanwhile build sum of distances.
-            summe = 0.0
-            modified = {}
-            for result in results:
-                formula_id = result['formula_id']
-                dtw = result['dtw']
-                if dtw == 0:
-                    logging.warning("Probability of 1!: %s" % str(formula_id))
-                    logging.warning(results)
-                    return [{'formula_id': formula_id, 'p': 1}]
-                else:
-                    modified[formula_id] = exp(-dtw)
-                    summe += modified[formula_id]
-
-            results = modified
-
+            distances = [result['dtw'] for result in results]
+            softmax_results = distance_metric.softmax(distances)
             probabilities = []
-            for formula_id, p in results.items():
-                probabilities.append({'formula_id': formula_id, 'p': p / summe})
+            for formula_id, p in zip(results, softmax_results):
+                probabilities.append({'formula_id': formula_id, 'p': p})
             return sorted(probabilities, key=lambda k: k['p'], reverse=True)
 
         return get_probability_from_distance(results)
---
+++ website/clients/python/distance_metric.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+
+import HandwrittenData
+import math
+import numpy
+
+
+def softmax(w, t=1.0):
+    """Calculate the softmax of a list of numbers w.
+    @param w: list of numbers
+    @return a list of the same length as w of non-negative numbers
+    >>> softmax([0.1, 0.2])
+    array([ 0.47502081,  0.52497919])
+    >>> softmax([-0.1, 0.2])
+    array([ 0.42555748,  0.57444252])
+    >>> softmax([0.9, -10])
+    array([  9.99981542e-01,   1.84578933e-05])
+    >>> softmax([0, 10])
+    array([  4.53978687e-05,   9.99954602e-01])
+    """
+    e = numpy.exp(numpy.array(w) / t)
+    dist = e / numpy.sum(e)
+    return dist
+
+
+def point_distance(p1, p2, squared=False):
+    """ Calculate the squared eucliden distance of two points.
+    @param  associative array $p1 first point
+    @param  associative array $p2 second point
+    @return float
+
+    >>> point_distance({'x': 0, 'y': 0}, {'x': 3, 'y': 4})
+    5.0
+    >>> '%.2f' % point_distance({'x': 0, 'y': 0}, {'x': 1, 'y': 22})
+    '22.02'
+    """
+    dx = p1["x"] - p2["x"]
+    dy = p1["y"] - p2["y"]
+    if squared:
+        return (dx*dx + dy*dy)
+    else:
+        return math.sqrt(dx*dx + dy*dy)
+
+
+def handwritten_data_greedy_matching_distance(a, b):
+    assert isinstance(a, HandwrittenData.HandwrittenData), \
+        "handwritten data is not of type HandwrittenData, but of %r" % \
+        type(a)
+    assert isinstance(b, HandwrittenData.HandwrittenData), \
+        "handwritten data is not of type HandwrittenData, but of %r" % \
+        type(b)
+
+    def sequence_greedy_distance(A, B, distance_func=point_distance):
+        """Calculate the distance between pointlist A and pointlist B.
+        @param A list of points; this list gets modified, so you should copy it
+                                 if you want to use it later
+        @param B list of points; this list gets modified, so you should copy it
+                                 if you want to use it later
+        @return non-negative float
+        """
+        assert isinstance(A, list), \
+            "A is not of type list, but of %r" % type(A)
+        assert isinstance(B, list), \
+            "B is not of type list, but of %r" % type(B)
+        assert len(A) > 0, "A is empty (%s)" % str(A)
+        assert len(B) > 0, "B is empty (%s)" % str(B)
+        d = 0
+        if len(A) == 1:
+            a2 = A.pop()
+            for p in B:
+                d = d + distance_func(a2, p)
+            return d
+        if len(B) == 1:
+            b2 = B.pop()
+            for p in A:
+                d = d + distance_func(b2, p)
+            return d
+        a = A.pop()
+        b = B.pop()
+        d = distance_func(a, b)
+        a2 = A.pop()
+        b2 = B.pop()
+        while len(A) > 0 and len(B) > 0:
+            l = distance_func(a2, b)
+            m = distance_func(a2, b2)
+            r = distance_func(a, b2)
+            mu = min(l, m, r)
+            d += mu
+            if (l == mu):
+                a = a2
+                a2 = A.pop()
+            elif (r == mu):
+                b = b2
+                b2 = B.pop()
+            else:
+                a = a2
+                b = b2
+                a2 = A.pop()
+                b2 = B.pop()
+        if len(A) == 0:
+            for p in B:
+                d += distance_func(a2, p)
+        elif len(B) == 0:
+            for p in A:
+                d += distance_func(b2, p)
+        return d
+
+    a_pointlist = a.get_pointlist()
+    b_pointlist = b.get_pointlist()
+
+    distance_sum = 0.0
+    if len(a_pointlist) != len(b_pointlist):
+        return float("inf")
+    else:
+        for lineA, lineB in zip(a_pointlist, b_pointlist):
+            distance_sum += sequence_greedy_distance(lineA[:], lineB[:])
+    return distance_sum
+
+if __name__ == '__main__':
+    pass
