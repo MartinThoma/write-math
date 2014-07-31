dea347e41b0c8efe2aa8c7c489f926cb0011eb1b
Martin Thoma <info@martin-thoma.de>
2014-07-31 14:05:46 -0400

    improve readability

--- website/clients/python/download_dataset.py
+++ website/clients/python/download_dataset.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+"""
+Download raw data from online server and store it as
+handwriting_datasets.pickle.
+"""
+
 
 import cPickle as pickle
 import MySQLdb
@@ -15,29 +20,36 @@ def main(cfg):
                                  cursorclass=MySQLdb.cursors.DictCursor)
     cursor = connection.cursor()
 
-    # Download all datasets
+    # Get all formulas that should get examined
     sql = ("SELECT `id`, `formula_in_latex` FROM `wm_formula` "
-           "WHERE `is_important` = 1 ORDER BY `id` ASC")
+           "WHERE `is_important` = 1 "  # only use the important symbol subset
+           "ORDER BY `id` ASC")
     cursor.execute(sql)
-    datasets = cursor.fetchall()
+    formulas = cursor.fetchall()
 
     handwriting_datasets = []
     formula_id2latex = {}
 
-    for dataset in datasets:
-        formula_id2latex[dataset['id']] = dataset['formula_in_latex']
+    # Go through each formula and download every raw_data instance
+    for formula in formulas:
+        formula_id2latex[formula['id']] = formula['formula_in_latex']
         sql = ("SELECT `id`, `data`, `is_in_testset` FROM `wm_raw_draw_data` "
-               "WHERE `accepted_formula_id` = %s" % str(dataset['id']))
+               "WHERE `accepted_formula_id` = %s" % str(formula['id']))
         cursor.execute(sql)
         raw_datasets = cursor.fetchall()
-        print("%s (%i)" % (dataset['formula_in_latex'], len(raw_datasets)))
+        print("%s (%i)" % (formula['formula_in_latex'], len(raw_datasets)))
         for raw_data in raw_datasets:
             try:
-                handwriting_datasets.append({'handwriting': HandwrittenData(raw_data['data']),
+                handwriting = HandwrittenData(raw_data['data'],
+                                              formula['id'],
+                                              raw_data['id']),
+                handwriting_datasets.append({'handwriting': handwriting,
                                              'id': raw_data['id'],
-                                             'formula_id': dataset['id'],
-                                             'formula_in_latex': dataset['formula_in_latex'],
-                                             'is_in_testset': raw_data['is_in_testset']
+                                             'formula_id': formula['id'],
+                                             'formula_in_latex':
+                                             formula['formula_in_latex'],
+                                             'is_in_testset':
+                                             raw_data['is_in_testset']
                                              })
             except Exception as e:
                 print("Raw data id: %s" % raw_data['id'])
