a7c622dbdf961b1f3fa87fc041e94d5e67df2814
Martin Thoma <info@martin-thoma.de>
2014-08-04 17:32:41 -0400

    misc

--- tools/analyze-data.py
+++ tools/analyze-data.py
@@ -55,7 +55,8 @@ def main(handwriting_datasets_file):
 
 
 def is_valid_file(parser, arg):
-    if not os.path.exists(os.path.abspath(arg)):
+    arg = os.path.abspath(arg)
+    if not os.path.exists(arg):
         parser.error("The file %s does not exist!" % arg)
     else:
         return arg
