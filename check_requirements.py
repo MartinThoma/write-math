a23f48495905d9613f5d4e5b27f8a8ef179b0e58
Martin Thoma <info@martin-thoma.de>
2014-08-05 10:53:12 -0400

    check for pfile_create

--- check_requirements.py
+++ check_requirements.py
@@ -2,23 +2,65 @@
 
 import imp
 
-required_modules = ['argparse', 'matplotlib']
-all_ok = True
 
-for required_module in required_modules:
-    try:
-        imp.find_module(required_module)
-        check = "module '%s' ... found" % required_module
-        if hasattr(required_module, '__version__'):
-            check += " (version: %s)" % str(required_module.__version__)
-        print(check)
+def which(program):
+    import os
 
-    except ImportError:
-        print("module '%s' ... NOT found" % required_module)
-        all_ok = False
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
 
-if all_ok:
-    import argparse
-    print("argparse version: %s (1.1 tested)" % argparse.__version__)
-    import matplotlib
-    print("matplotlib version: %s (1.2.1 tested)" % matplotlib.__version__)
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
+
+def check_python_modules():
+    print("## Check modules")
+    required_modules = ['argparse', 'matplotlib', 'detl']
+    all_ok = True
+
+    for required_module in required_modules:
+        try:
+            imp.find_module(required_module)
+            check = "module '%s' ... found" % required_module
+            if hasattr(required_module, '__version__'):
+                check += " (version: %s)" % str(required_module.__version__)
+            print(check)
+
+        except ImportError:
+            print("module '%s' ... NOT found" % required_module)
+            all_ok = False
+
+    if all_ok:
+        import argparse
+        print("argparse version: %s (1.1 tested)" % argparse.__version__)
+        import matplotlib
+        print("matplotlib version: %s (1.2.1 tested)" % matplotlib.__version__)
+
+
+def check_executables():
+    print("## Check executables")
+    required_executables = ["pfile_create"]
+    for executable in required_executables:
+        path = which(executable)
+        if path is None:
+            print("%s ... NOT found" % executable)
+        else:
+            print("%s ... found at %s" % (executable, path))
+
+
+def main():
+    check_python_modules()
+    check_executables()
+
+if __name__ == '__main__':
+    main()
