9dcca3d47fc7c0bbcd94b64e95bc46e06df2a03b
Martin Thoma <info@martin-thoma.de>
2014-08-05 11:01:42 -0400

    added 'time between points' analyzation

--- tools/analyze-data.py
+++ tools/analyze-data.py
@@ -15,6 +15,7 @@ import cPickle as pickle
 import time
 import datetime
 from HandwrittenData import HandwrittenData  # Needed because of pickle
+import numpy
 
 
 def get_bounding_box_sizes(raw_datasets):
@@ -44,13 +45,47 @@ def get_bounding_box_sizes(raw_datasets):
     return bouding_box_sizes
 
 
+def get_time_between_controll_points(raw_datasets):
+    average_between_points = open("average_time_between_points.txt", "a")
+    average_between_lines = open("average_time_between_lines.txt", "a")
+    start_time = time.time()
+    for i, raw_dataset in enumerate(raw_datasets):
+        if i % 100 == 0 and i > 0:
+            # Show how much work was done / how much work is remaining
+            percentage_done = float(i)/len(raw_datasets)
+            current_running_time = time.time() - start_time
+            remaining_seconds = current_running_time / percentage_done
+            tmp = datetime.timedelta(seconds=remaining_seconds)
+            sys.stdout.write("\r%0.2f%% (%s remaining)   " %
+                             (percentage_done*100, str(tmp)))
+            sys.stdout.flush()
+        # Do the work
+        times_between_points = []
+        times_between_lines = []
+        last_line_end = None
+        for line in raw_dataset['handwriting'].get_pointlist():
+            if last_line_end is not None:
+                times_between_lines.append(line[-1]['time'] - last_line_end)
+            last_line_end = line[-1]['time']
+            last_point_end = None
+            for point in line:
+                if last_point_end is not None:
+                    times_between_points.append(point['time'] - last_point_end)
+                last_point_end = point['time']
+        average_between_points.write("%0.2f\n" % numpy.average(times_between_points))
+        average_between_lines.write("%0.2f\n" % numpy.average(times_between_lines))
+
+    average_between_points.close()
+    average_between_lines.close()
+
+
 def main(handwriting_datasets_file):
     # Load from pickled file
     logging.info("Start loading data...")
     loaded = pickle.load(open(handwriting_datasets_file))
     raw_datasets = loaded['handwriting_datasets']
     logging.info("Start analyzing")
-    get_bounding_box_sizes(raw_datasets)
+    get_time_between_controll_points(raw_datasets)
 
 
 def is_valid_file(parser, arg):
@@ -71,7 +106,7 @@ if __name__ == '__main__':
                         metavar="FILE",
                         type=lambda x: is_valid_file(parser, x),
                         default=("../archive/datasets/"
-                                 "2014-08-03-18-06-"
+                                 "2014-08-04-18-24-"
                                  "handwriting_datasets-raw.pickle"))
     args = parser.parse_args()
     main(args.handwriting_datasets)
