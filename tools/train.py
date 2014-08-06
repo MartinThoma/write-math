3ae77660e743acf587c9b0346c511546a45c971c
Martin Thoma <info@martin-thoma.de>
2014-08-05 23:59:39 -0400

    fixed error

--- tools/train.py
+++ tools/train.py
@@ -31,7 +31,7 @@ def create_model(model_folder, basename, model_type, topology, training,
         os.system(command)
     else:
         latest_model = models[0]
-        i = int(latest_model.split("-")[-1])
+        i = int(latest_model.split("-")[-1].split(".")[0])
         model_src = os.path.json(model_folder, "%s-%i.json" % (basename, i))
         model_target = os.path.json(model_folder,
                                     "%s-%i.json" % (basename, i+1))
