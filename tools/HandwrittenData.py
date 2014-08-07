7fcefd07a3274d847aa5ceb009e6f1ccc38303b6
Martin Thoma <info@martin-thoma.de>
2014-08-07 01:04:14 -0400

    added docstrings; formatting

--- tools/HandwrittenData.py
+++ tools/HandwrittenData.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Representation of a recording (handwritten data)."""
+
 import logging
 import json
 import matplotlib.pyplot as plt
@@ -47,6 +49,11 @@ class HandwrittenData(object):
         return pointlist
 
     def set_pointlist(self, pointlist):
+        """Overwrite pointlist.
+        @param pointlist - a list of lists. The inner lists represent strokes.
+                           Every stroke consists of points.
+                           Every point is a dictinary with 'x', 'y', 'time'.
+        """
         assert type(pointlist) is list, \
             "pointlist is not of type list, but %r" % type(pointlist)
         assert len(pointlist) >= 1, \
@@ -109,13 +116,12 @@ class HandwrittenData(object):
         return features
 
     def show(self):
+        """Show the data graphically in a new pop-up window."""
         pointlist = self.get_pointlist()
         if 'pen_down' in pointlist[0][0]:
-            if len(pointlist) > 1:
-                print("Lenght of pointlist was %i." % len(pointlist))
-                print("That was unexpected")
-                print(pointlist)
-                exit()
+            assert len(pointlist) > 1, \
+                "Lenght of pointlist was %i. Got: %s" % (len(pointlist),
+                                                         pointlist)
             new_pointlist = []
             last = None
             stroke = []
@@ -131,10 +137,10 @@ class HandwrittenData(object):
             new_pointlist.append(stroke)
             pointlist = new_pointlist
 
-        fig, ax = plt.subplots()
-        ax.set_title('Raw data id: %s, Formula_id: %s' % (str(self.raw_data_id),
-                                                          str(self.formula_id)
-                                                          ))
+        _, ax = plt.subplots()
+        ax.set_title("Raw data id: %s, "
+                     "Formula_id: %s" % (str(self.raw_data_id),
+                                         str(self.formula_id)))
 
         for line in pointlist:
             xs, ys = [], []
@@ -165,38 +171,50 @@ class HandwrittenData(object):
         return self.__repr__()
 
 if __name__ == '__main__':
-    a = HandwrittenData('[[{"x":305,"y":260,"time":35},'
-                        '{"x":305,"y":264,"time":43},'
-                        '{"x":306,"y":270,"time":52},'
-                        '{"x":306,"y":276,"time":59},'
-                        '{"x":306,"y":283,"time":67},'
-                        '{"x":306,"y":291,"time":75},'
-                        '{"x":306,"y":299,"time":83},'
-                        '{"x":306,"y":307,"time":91},'
-                        '{"x":306,"y":315,"time":99},'
-                        '{"x":306,"y":322,"time":107},'
-                        '{"x":306,"y":330,"time":115},'
-                        '{"x":308,"y":338,"time":123},'
-                        '{"x":308,"y":344,"time":132},'
-                        '{"x":308,"y":350,"time":140},'
-                        '{"x":308,"y":356,"time":149},'
-                        '{"x":308,"y":362,"time":156},'
-                        '{"x":308,"y":368,"time":164},'
-                        '{"x":308,"y":372,"time":172},'
-                        '{"x":308,"y":376,"time":180},'
-                        '{"x":308,"y":380,"time":187},'
-                        '{"x":308,"y":381,"time":196},'
-                        '{"x":308,"y":382,"time":205},'
-                        '{"x":308,"y":383,"time":212},'
-                        '{"x":308,"y":384,"time":219}],'
-                        '[{"x":362,"y":269,"time":6},'
-                        '{"x":360,"y":271,"time":11},'
-                        '{"x":356,"y":275,"time":19},'
-                        '{"x":352,"y":279,"time":27},'
-                        '{"x":348,"y":283,"time":35},'
-                        '{"x":344,"y":287,"time":43},'
-                        '{"x":340,"y":291,"time":51},'
-                        '{"x":336,"y":296,"time":59},'
-                        '{"x":334,"y":302,"time":67},'
-                        '{"x":330,"y":308,"time":75},{"x":326,"y":312,"time":83},{"x":322,"y":316,"time":91},{"x":318,"y":320,"time":99},{"x":316,"y":324,"time":107},{"x":314,"y":325,"time":115},{"x":313,"y":327,"time":123},{"x":311,"y":328,"time":131},{"x":309,"y":329,"time":139},{"x":308,"y":329,"time":147},{"x":307,"y":330,"time":155},{"x":306,"y":330,"time":172},{"x":306,"y":331,"time":180},{"x":305,"y":331,"time":188},{"x":304,"y":331,"time":203},{"x":306,"y":332,"time":325},{"x":308,"y":332,"time":333},{"x":309,"y":333,"time":339},{"x":312,"y":334,"time":347},{"x":314,"y":335,"time":355},{"x":316,"y":337,"time":363},{"x":318,"y":338,"time":372},{"x":320,"y":340,"time":379},{"x":322,"y":342,"time":387},{"x":325,"y":344,"time":396},{"x":327,"y":347,"time":403},{"x":329,"y":349,"time":411},{"x":330,"y":351,"time":419},{"x":332,"y":352,"time":427},{"x":333,"y":354,"time":435},{"x":335,"y":355,"time":443},{"x":336,"y":357,"time":451},{"x":337,"y":358,"time":459},{"x":338,"y":360,"time":468},{"x":339,"y":361,"time":476},{"x":340,"y":362,"time":484}]]')
-    a.show()
+    EXAMPLE = HandwrittenData('[[{"x":305,"y":260,"time":35},'
+                              '{"x":305,"y":264,"time":43},'
+                              '{"x":306,"y":270,"time":52},'
+                              '{"x":306,"y":276,"time":59},'
+                              '{"x":306,"y":283,"time":67},'
+                              '{"x":306,"y":291,"time":75},'
+                              '{"x":306,"y":299,"time":83},'
+                              '{"x":306,"y":307,"time":91},'
+                              '{"x":306,"y":315,"time":99},'
+                              '{"x":306,"y":322,"time":107},'
+                              '{"x":306,"y":330,"time":115},'
+                              '{"x":308,"y":338,"time":123},'
+                              '{"x":308,"y":344,"time":132},'
+                              '{"x":308,"y":350,"time":140},'
+                              '{"x":308,"y":356,"time":149},'
+                              '{"x":308,"y":362,"time":156},'
+                              '{"x":308,"y":368,"time":164},'
+                              '{"x":308,"y":372,"time":172},'
+                              '{"x":308,"y":376,"time":180},'
+                              '{"x":308,"y":380,"time":187},'
+                              '{"x":308,"y":381,"time":196},'
+                              '{"x":308,"y":382,"time":205},'
+                              '{"x":308,"y":383,"time":212},'
+                              '{"x":308,"y":384,"time":219}],'
+                              '[{"x":362,"y":269,"time":6},'
+                              '{"x":360,"y":271,"time":11},'
+                              '{"x":356,"y":275,"time":19},'
+                              '{"x":352,"y":279,"time":27},'
+                              '{"x":348,"y":283,"time":35},'
+                              '{"x":344,"y":287,"time":43},'
+                              '{"x":340,"y":291,"time":51},'
+                              '{"x":336,"y":296,"time":59},'
+                              '{"x":334,"y":302,"time":67},'
+                              '{"x":330,"y":308,"time":75},'
+                              '{"x":326,"y":312,"time":83},'
+                              '{"x":322,"y":316,"time":91},'
+                              '{"x":318,"y":320,"time":99},'
+                              '{"x":316,"y":324,"time":107},'
+                              '{"x":314,"y":325,"time":115},'
+                              '{"x":313,"y":327,"time":123},'
+                              '{"x":311,"y":328,"time":131},'
+                              '{"x":309,"y":329,"time":139},'
+                              '{"x":308,"y":329,"time":147},'
+                              '{"x":307,"y":330,"time":155},'
+                              '{"x":306,"y":330,"time":172},'
+                              '{"x":306,"y":331,"time":180},{"x":305,"y":331,"time":188},{"x":304,"y":331,"time":203},{"x":306,"y":332,"time":325},{"x":308,"y":332,"time":333},{"x":309,"y":333,"time":339},{"x":312,"y":334,"time":347},{"x":314,"y":335,"time":355},{"x":316,"y":337,"time":363},{"x":318,"y":338,"time":372},{"x":320,"y":340,"time":379},{"x":322,"y":342,"time":387},{"x":325,"y":344,"time":396},{"x":327,"y":347,"time":403},{"x":329,"y":349,"time":411},{"x":330,"y":351,"time":419},{"x":332,"y":352,"time":427},{"x":333,"y":354,"time":435},{"x":335,"y":355,"time":443},{"x":336,"y":357,"time":451},{"x":337,"y":358,"time":459},{"x":338,"y":360,"time":468},{"x":339,"y":361,"time":476},{"x":340,"y":362,"time":484}]]')
+    EXAMPLE.show()
