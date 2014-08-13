cb9a228eda8cc6ee8b4cda25aa67a6387a6a842d
Martin Thoma <info@martin-thoma.de>
2014-08-12 23:35:48 -0400

    added code to use preprocessing queue from latest model file

--- tools/view.py
+++ tools/view.py
@@ -3,14 +3,66 @@
 Display a raw_data_id.
 """
 
+import os
+import yaml
+# mine
 from HandwrittenData import HandwrittenData
 import MySQLdb
 import MySQLdb.cursors
 import utils
+import preprocessing
 
 
-def main(s):
-    a = HandwrittenData(s)
+def fetch_data(raw_data_id):
+    """Get the data from raw_data_id from the server."""
+    # Import configuration file
+    cfg = utils.get_database_configuration()
+
+    # Establish database connection
+    connection = MySQLdb.connect(host=cfg[args.mysql]['host'],
+                                 user=cfg[args.mysql]['user'],
+                                 passwd=cfg[args.mysql]['passwd'],
+                                 db=cfg[args.mysql]['db'],
+                                 cursorclass=MySQLdb.cursors.DictCursor)
+    cursor = connection.cursor()
+
+    # Download dataset
+    sql = ("SELECT `id`, `data` "
+           "FROM `wm_raw_draw_data` WHERE `id`=%i") % args.id
+    cursor.execute(sql)
+    data = cursor.fetchone()
+    return data
+
+
+def display_data(raw_data_string, raw_data_id):
+    PROJECT_ROOT = utils.get_project_root()
+
+    # Get latest model description file
+    models_folder = os.path.join(PROJECT_ROOT, "archive/models")
+    latest_model = utils.get_latest_in_folder(models_folder, ".yml")
+
+    # Read the model description file
+    with open(latest_model, 'r') as ymlfile:
+        model_description = yaml.load(ymlfile)
+
+    # Get the preprocessing queue
+    preprocessing_queue = []
+    print(model_description['preprocessing'])
+    for el in model_description['preprocessing']:
+        parameters = {}
+        algorithms = el.keys()
+        for algorithm in algorithms:
+            parameters = {}
+            if el[algorithm] is not None:
+                for param in el[algorithm]:
+                    for key in param.keys():
+                        print(key)
+                        parameters[key] = param[key]
+            algorithm = preprocessing.get_algorithm(algorithm)
+            preprocessing_queue.append((algorithm, parameters))
+
+    a = HandwrittenData(raw_data_string, raw_data_id=raw_data_id)
+    a.preprocessing(preprocessing_queue)
     a.show()
 
 if __name__ == '__main__':
@@ -23,22 +75,8 @@ if __name__ == '__main__':
                         help="which mysql configuration should be used?")
     args = parser.parse_args()
 
-    # Import configuration file
-    cfg = utils.get_database_configuration()
-
-    # Establish database connection
-    connection = MySQLdb.connect(host=cfg[args.mysql]['host'],
-                                 user=cfg[args.mysql]['user'],
-                                 passwd=cfg[args.mysql]['passwd'],
-                                 db=cfg[args.mysql]['db'],
-                                 cursorclass=MySQLdb.cursors.DictCursor)
-    cursor = connection.cursor()
-
-    # Download all datasets
-    sql = "SELECT `data` FROM `wm_raw_draw_data` WHERE `id`=%i" % args.id
-    cursor.execute(sql)
-    data = cursor.fetchone()
+    data = fetch_data(args.id)
     if data is None:
         print("RAW_DATA_ID %i does not exist." % args.id)
     else:
-        main(data['data'])
+        display_data(data['data'], data['id'])
