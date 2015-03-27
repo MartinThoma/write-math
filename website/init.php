35c7c037dca5445edcb20571b76d251e0b03325b
Martin Thoma <info@martin-thoma.de>
2015-03-27 14:17:36 +0100

    init.php: Start session only if necessary; include session.php only once

--- website/init.php
+++ website/init.php
@@ -1,6 +1,8 @@
 <?php
 require_once dirname(__FILE__).'/vendor/autoload.php';
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 require("config.php");
 $dsn = "mysql:host=$server;dbname=$dbname";
 
@@ -11,7 +13,7 @@ try {
     echo 'Connection failed: ' . $e->getMessage();
 }
 
-include 'session.php';
+include_once 'session.php';
 
 $loader = new Twig_Loader_Filesystem(dirname(__FILE__).'/templates');
 $twig = new Twig_Environment($loader, array(
