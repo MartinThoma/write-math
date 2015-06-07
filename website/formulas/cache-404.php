642991c5e121c4a8277a5f322fd139359bfa18da
Martin Thoma <info@martin-thoma.de>
2015-06-07 19:56:21 +0200

    formulas/cache-404: fix warning

--- website/formulas/cache-404.php
+++ website/formulas/cache-404.php
@@ -9,7 +9,13 @@ $sql = "SELECT svg FROM `wm_renderings` WHERE id=:rid";
 $stmt = $pdo->prepare($sql);
 $stmt->bindParam(':rid', $rendering_id, PDO::PARAM_INT);
 $stmt->execute();
-$svg = $stmt->fetchObject()->svg;
+$svg_obj = $stmt->fetchObject();
+if ($svg_obj) {
+    $svg = $svg_obj->svg;
+} else {
+    $svg = "";
+}
+
 
 if (strlen($svg) > 50) {
     file_put_contents ("../formulas/$formula_id-$rendering_id.svg", $svg);
