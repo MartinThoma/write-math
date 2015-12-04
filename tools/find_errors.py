57aad613de40813c652dfb145d3375ac988fbe11
Martin Thoma <info@martin-thoma.de>
2015-12-04 18:33:26 +0100

    Avoid non-python file names and code duplication

--- tools/find_errors.py
+++ tools/find_errors.py
@@ -21,10 +21,10 @@ import pymysql.cursors
 import webbrowser
 
 # My modules
-from hwrt.handwritten_data import HandwrittenData
 from distance_metric import handwritten_data_greedy_matching_distance as dtw
 from hwrt import preprocessing
 from hwrt import utils
+from misc import HandwrittenDataM
 
 
 def update_data(cfg, a, unaccept=False):
@@ -107,114 +107,6 @@ def update_data(cfg, a, unaccept=False):
     connection_online.close()
 
 
-class HandwrittenDataM(HandwrittenData):
-    def __init__(self,
-                 raw_data_json,
-                 formula_id,
-                 wild_point_count,
-                 missing_line,
-                 has_hook,
-                 has_too_long_line,
-                 is_image,
-                 other_problem,
-                 has_interrupted_line,
-                 raw_data_id,
-                 latex):
-        HandwrittenData.__init__(self, raw_data_json, formula_id)
-        self.wild_point_count = wild_point_count
-        self.missing_line = missing_line
-        self.has_hook = has_hook
-        self.has_too_long_line = has_too_long_line
-        self.is_image = is_image
-        self.other_problem = other_problem
-        self.has_interrupted_line = has_interrupted_line
-        self.unaccept = False
-        self.ok = False
-        self.raw_data_id = raw_data_id
-        self.latex = latex
-        self.istrash = False
-
-    def show(self):
-        import matplotlib.pyplot as plt
-        from matplotlib.widgets import CheckButtons
-        from matplotlib.widgets import Button
-        fig, ax = plt.subplots()
-        ax.set_title('Raw data id: %i, LaTeX: %s' % (self.raw_data_id,
-                                                     self.latex))
-        plt.subplots_adjust(bottom=0.3)
-
-        for line in self.get_pointlist():
-            xs, ys = [], []
-            for p in line:
-                xs.append(p['x'])
-                ys.append(p['y'])
-            ax.plot(xs, ys, '-o')
-        ax.axis((0, 1, 0, 1))
-        plt.gca().invert_yaxis()
-        ax.set_aspect('equal')
-
-        def handle_checkboxes(label):
-            if label == 'other_problem':
-                self.other_problem = not self.other_problem
-            elif label == 'missing_line':
-                self.missing_line = not self.missing_line
-            elif label == 'has_hook':
-                self.has_hook = not self.has_hook
-            elif label == 'has_too_long_line':
-                self.has_too_long_line = not self.has_too_long_line
-            elif label == 'is_image':
-                self.is_image = not self.is_image
-            elif label == 'has_interrupted_line':
-                self.has_interrupted_line = not self.has_interrupted_line
-            plt.draw()
-
-        checkpos = plt.axes([0.01, 0.05, 0.45, 0.2])
-        check = CheckButtons(checkpos,
-                             ('other_problem',
-                              'has_interrupted_line',
-                              'missing_line',
-                              'has_hook',
-                              'has_too_long_line',
-                              'is_image'),
-                             (self.other_problem,
-                              self.has_interrupted_line,
-                              self.missing_line,
-                              self.has_hook,
-                              self.has_too_long_line,
-                              self.is_image))
-        check.on_clicked(handle_checkboxes)
-
-        def ok_function(a):
-            self.ok = True
-            plt.close()
-        okpos = plt.axes([0.7, 0.05, 0.1, 0.075])
-        ok_button = Button(okpos, 'OK')
-        ok_button.on_clicked(ok_function)
-
-        def unaccept_function(a):
-            self.unaccept = True
-            logger.info("unaccept raw_data_id %i" % self.raw_data_id)
-            plt.close()
-        unacceptpos = plt.axes([0.81, 0.05, 0.1, 0.075])
-        unaccept_button = Button(unacceptpos, 'unaccept')
-        unaccept_button.on_clicked(unaccept_function)
-
-        def is_trash_function(a):
-            a.istrash = True
-            logger.info("trash raw_data_id %i" % self.raw_data_id)
-            plt.close()
-        is_trash_pos = plt.axes([0.7, 0.2, 0.1, 0.075])
-        is_trash_button = Button(is_trash_pos, 'is_trash')
-        is_trash_button.on_clicked(is_trash_function)
-
-        # maximise height
-        mng = plt.get_current_fig_manager()
-        width, height = mng.window.maxsize()
-        mng.resize(500, height)
-        plt.axis('equal')
-        plt.show()
-
-
 def main(cfg, raw_data_start_id):
     mysql = cfg['mysql_online']
     connection = pymysql.connect(host=mysql['host'],
@@ -267,11 +159,11 @@ def main(cfg, raw_data_start_id):
         checked_formulas += 1
         if len(raw_datasets) < 100:
             continue
-        As = []
+        as_ = []
         for i, data in enumerate(raw_datasets):
             if data['data'] == "[]":
                 continue
-            B = HandwrittenDataM(data['data'],
+            b = HandwrittenDataM(data['data'],
                                  data['accepted_formula_id'],
                                  data['wild_point_count'],
                                  data['missing_line'],
@@ -282,26 +174,26 @@ def main(cfg, raw_data_start_id):
                                  data['has_interrupted_line'],
                                  data['id'],
                                  formulaid2latex[formula_id])
-            B.preprocessing(preprocessing_queue)
-            B_pll = B.get_pointlist()
+            b.preprocessing(preprocessing_queue)
+            b_pll = b.get_pointlist()
             distance = float('inf')
-            for A_pll in As:
-                distance = min(distance, dtw(A_pll, B_pll))
+            for A_pll in as_:
+                distance = min(distance, dtw(A_pll, b_pll))
             if distance > 100:
                 if data['administrator_edit'] is not None:
-                    As.append(B.get_pointlist())
+                    as_.append(b.get_pointlist())
                 else:
                     if not alread_shown_in_browser:
                         alread_shown_in_browser = True
                         webbrowser.open("http://www.martin-thoma.de/"
                                         "write-math/view/?"
                                         "raw_data_id=%i" % data['id'], 2)
-                    B.show()
-                    if B.ok:
-                        As.append(B.get_pointlist())
-                        update_data(cfg, B)
+                    b.show()
+                    if b.ok:
+                        as_.append(b.get_pointlist())
+                        update_data(cfg, b)
                     else:
-                        update_data(cfg, B, True)
+                        update_data(cfg, b, True)
         logger.info("[Status] Checked formulas: %i of %i" %
                     (checked_formulas, len(formulaid2latex)))
         logger.info("[Status] Checked raw_data_instances: %i" %
